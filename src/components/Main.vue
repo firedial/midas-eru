c428b4f170e2b1c62fe7c189e08812a69e476b91
firedial <42674772+firedial@users.noreply.github.com>
2020-05-02 20:29:11 +0900

    API をつかってデータ取得部分の実装

---
+++ src/components/Main.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="hello">
+    aaa
+    <button v-on:click="getBalances">get</button>
+    <balance-table v-bind:balances="balances"/>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import Table from '@/components/Table'
+export default {
+  name: 'Main',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      balances: []
+    }
+  },
+  methods: {
+    getBalances: function () {
+      axios
+        .get('http://localhost:8080/api/v1/balance/?')
+        .then(response => (this.balances = response.data))
+    }
+  },
+  components: {
+    'balance-table': Table
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/Table.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="hello">
+    <table>
+      <tr>
+        <th>balance_id</th>
+        <th>amount</th>
+        <th>item</th>
+        <th>kind_id</th>
+        <th>purpose_id</th>
+        <th>place_id</th>
+        <th>date</th>
+      </tr>
+      <tr v-for="balance in balances" v-bind:key="balance.balance_id">
+        <td> {{ balance.balance_id }} </td>
+        <td> {{ balance.amount }} </td>
+        <td> {{ balance.item }} </td>
+        <td> {{ balance.kind_id }} </td>
+        <td> {{ balance.purpose_id }} </td>
+        <td> {{ balance.place_id }} </td>
+        <td> {{ balance.date }} </td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Table',
+  props: ['balances']
+}
+</script>
