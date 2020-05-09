e6f13a5f06b52031ae0c40221edcab8f16a3f66f
firedial <42674772+firedial@users.noreply.github.com>
2020-05-09 13:36:25 +0900

    key-bind の設定

--- src/components/SumTable.vue
+++ src/components/SumTable.vue
@@ -4,12 +4,13 @@
     <table>
       <tr>
         <th>id</th>
+        <th>attribute</th>
         <th>amount</th>
         <th>date</th>
       </tr>
-      <!-- tr v-for="sum in sums" v-bind:key="sum.id" -->
-      <tr v-for="sum in sums">
+      <tr v-for="sum in sums" v-bind:key="sum.id">
         <td> {{ sum.id }} </td>
+        <td> {{ sum.attribute_id }} </td>
         <td> {{ sum.amount_sum }} </td>
         <td> {{ sum.date }} </td>
       </tr>
